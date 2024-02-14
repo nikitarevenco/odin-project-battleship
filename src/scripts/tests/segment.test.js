946f72bb76510c9c6793ffc1f7707ee12cf1a046
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-02-14 14:37:41 +0000

    feat(segment): add basic functionality

---
+++ src/scripts/tests/segment.test.js
@@ -0,0 +1,34 @@
+import Segment from "../segment";
+
+describe("Testing segment class", () => {
+  const seg = new Segment();
+  it("Returns nothing when hit for the first time", () => {
+    expect(seg.kill()).toBe(undefined);
+  });
+  it("Throws error when hit again or anytime after that", () => {
+    expect(() => {
+      seg.kill();
+    }).toThrow("This segment has already been hit");
+    expect(() => {
+      seg.kill();
+    }).toThrow("This segment has already been hit");
+    expect(() => {
+      seg.kill();
+    }).toThrow("This segment has already been hit");
+    expect(() => {
+      seg.kill();
+    }).toThrow("This segment has already been hit");
+    expect(() => {
+      seg.kill();
+    }).toThrow("This segment has already been hit");
+    expect(() => {
+      seg.kill();
+    }).toThrow("This segment has already been hit");
+    expect(() => {
+      seg.kill();
+    }).toThrow("This segment has already been hit");
+    expect(() => {
+      seg.kill();
+    }).toThrow("This segment has already been hit");
+  });
+});
