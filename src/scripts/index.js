ed44928ef27c5e5b201eff016e568b2f598f5b7d
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-02-17 10:17:43 +0000

    feat: add bot as enemy

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 import Player from "./player";
 import updateDomBoard from "./dom-board-manager";
 import updateDomShips from "./dom-ship-manager";
+import Bot from "./ai";
 
 function importAllCSS(r) {
   r.keys().forEach(r);
@@ -8,7 +9,12 @@ function importAllCSS(r) {
 importAllCSS(require.context("../styles/", true, /\.css$/));
 
 const bob = new Player("bob", null, true);
+const chatgpt = new Bot("chatgpt", bob, false);
+bob.enemy = chatgpt;
 const domBoard = document.querySelector("#board");
 const domShips = document.querySelector("#ships");
 updateDomBoard(bob, domBoard);
+// updateDomBoard(bob, domBoard);
 updateDomShips(bob.unplacedShips, domShips);
+
+export default chatgpt;
